81b58f91b9d3f755718f409fe6a8510e3859bd48
safiraferia <safiraamalina85@gmail.com>
2024-07-25 02:39:27 +0700

    Update stream-diabetes.py

--- stream-diabetes.py
+++ stream-diabetes.py
@@ -42,9 +42,9 @@ if st.button('Test Prediksi Diabetes'):
     diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
 
     if(diab_prediction[0] == 1):
-        diab_diagnosis = 'Pasien terkena Diabetes'
+        diab_diagnosis = 'Pasien Terkena Diabetes'
     else:
-        diab_diagnosis = 'Pasien tidak terkena Diabetes'
+        diab_diagnosis = 'Pasien Tidak Terkena Diabetes'
 
 # Menampilkan hasil di tengah
 st.markdown(f"""
